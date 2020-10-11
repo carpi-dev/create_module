fc58d3e8d954dbf9d22af8d66010fc77dfabd148
Pascal Eberlein <pascal@eberlein.io>
2020-10-11 18:23:55 +0200

    added {2}_LOADABLE definition

--- create_module.py
+++ create_module.py
@@ -13,6 +13,7 @@ if(CMAKE_VERSION VERSION_LESS "3.7.0")
 endif()
 
 set(CMAKE_CXX_STANDARD 11)
+add_compile_definitions({2}_LOADABLE)
 
 set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
@@ -35,20 +36,30 @@ install(TARGETS {1}
 
 
 TPL_CPP = """#include "{0}.h"
+
+#ifdef {2}_LOADABLE
 #include "ui_{1}.h"
+#endif
 
-
-{0}::{0}(QWidget *parent): QWidget(parent), ui(new Ui::{0})
+{0}::{0}(QWidget *parent): QWidget(parent), 
+#ifdef {2}_LOADABLE
+ui(new Ui::{0})
+#endif
 {
+#ifdef {2}_LOADABLE
     ui->setupUi(this);
+#endif
     // todo signals and slots for labels
 }
 
 {0}::~{0}()
 {
+#ifdef {2}_LOADABLE
     delete ui;
+#endif
 }
 
+#ifdef {2}_LOADABLE
 extern "C" {2}_EXPORT QWidget* create() {
     return new {0}();
 }
@@ -72,30 +83,38 @@ extern "C" {2}_EXPORT QStringList getDependencies(){
 extern "C" {2}_EXPORT int getVersion(){
     return 1;
 }
+#endif
 """
 
 TPL_H = """#ifndef CARPI_QT_{2}_H
 #define CARPI_QT_{2}_H
 
+#ifdef {2}_LOADABLE
 #include "{0}_global.h"
+#endif
 
 #include <QWidget>
 
+#ifdef {2}_LOADABLE
 QT_BEGIN_NAMESPACE
 namespace Ui { class {0}; }
 QT_END_NAMESPACE
+#endif
 
 class {0} : public QWidget
 {
 Q_OBJECT
 private:
+#ifdef
     Ui::{0} *ui;
+#endif
 
 public:
     explicit {0}(QWidget *parent = nullptr);
     ~{0}() override;
 };
 
+#ifdef {2}_LOADABLE
 extern "C" {
     {2}_EXPORT int getDefaultIndex();
     {2}_EXPORT char* getName();
@@ -104,7 +123,7 @@ extern "C" {
     {2}_EXPORT QStringList getSettingsKeys();
     {2}_EXPORT QStringList getDependencies();
 };
-
+#endif
 
 #endif //CARPI_QT_{2}_H
 """
